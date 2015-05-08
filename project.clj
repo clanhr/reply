fd3f64c5e846fee19883b910eabb725839f2fa9c
Pedro Santos <donbonifacio@gmail.com>
2015-05-08 14:59:02 +0100

    v0.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clanhr/reply "0.1.0"
+(defproject clanhr/reply "0.2.0"
   :description "FIXME: write description"
   :url "http://github.com/clanhr/reply"
   :license {:name "Eclipse Public License"
