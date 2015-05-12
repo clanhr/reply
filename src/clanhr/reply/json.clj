d31e7cad849d7e497530887bdb8d3c7ea4669ca6
Pedro Santos <donbonifacio@gmail.com>
2015-05-12 11:34:52 +0100

    Serialize dates

--- src/clanhr/reply/json.clj
+++ src/clanhr/reply/json.clj
@@ -1,6 +1,12 @@
 (ns clanhr.reply.json
   (:require [cheshire.core :refer :all]
-            [cheshire.generate :refer [add-encoder encode-str]]))
+            [cheshire.generate :refer [add-encoder encode-str]]
+            [clj-time.coerce :as coerce]))
+
+(add-encoder
+  org.joda.time.DateTime
+  (fn [data jsonGenerator]
+    (.writeString jsonGenerator (coerce/to-string data))))
 
 (defn dump
   "Dumps the given object as json"
@@ -11,3 +17,4 @@
   "Loads the json as edn"
   [raw]
   (parse-string raw true))
+
