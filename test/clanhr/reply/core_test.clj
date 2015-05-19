983ba56a09925cece29b940ab8560cc65a1db6ae
Pedro Santos <donbonifacio@gmail.com>
2015-05-19 10:20:32 +0100

    Fix indenting

--- test/clanhr/reply/core_test.clj
+++ test/clanhr/reply/core_test.clj
@@ -6,49 +6,49 @@
 (deftest ok-test
   (let [data {:company "ClanHR"}
         response (reply/ok data)]
-  (is (= 200 (:status response)))))
+    (is (= 200 (:status response)))))
 
 (deftest created-test
   (let [data {:company "ClanHR"}
         response (reply/created data)]
-  (is (= 201 (:status response)))))
+    (is (= 201 (:status response)))))
 
 (deftest saved-test
   (let [data {:company "ClanHR"}
         response (reply/saved true data)]
-  (is (= 201 (:status response))))
+    (is (= 201 (:status response))))
   (let [data {:company "ClanHR"}
         response (reply/saved false data)]
-  (is (= 200 (:status response)))))
+    (is (= 200 (:status response)))))
 
 (deftest accepted-test
   (let [data {:company "ClanHR"}
         response (reply/accepted data)]
-  (is (= 202 (:status response)))))
+    (is (= 202 (:status response)))))
 
 (deftest bad-request-test
   (let [data {:company "ClanHR"}
         response (reply/bad-request data)]
-  (is (= 400 (:status response)))))
+    (is (= 400 (:status response)))))
 
 (deftest unauthorized
   (let [response (reply/unauthorized)]
-  (is (= 401 (:status response)))))
+    (is (= 401 (:status response)))))
 
 (deftest not-found
   (let [data {:company "ClanHR"}
         response (reply/not-found data)]
-  (is (= 404 (:status response)))))
+    (is (= 404 (:status response)))))
 
 (deftest internal-server-error
   (let [data {:company "ClanHR"}
         response (reply/internal-server-error data)]
-  (is (= 500 (:status response)))))
+    (is (= 500 (:status response)))))
 
 (deftest render-dates
   (let [data {:date (t/now)}
         response (reply/ok data)]
-  (is (= 200 (:status response)))))
+    (is (= 200 (:status response)))))
 
 (deftest result-test
   (let [response-ok (reply/result {:success true})
